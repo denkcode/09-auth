1034ce4a18e3602c8b0d6114d877480be98e84dc
fqsocite <denispenzinn@gmail.com>
2026-04-17 23:34:16 +0200

    m

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useParams } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
-import { fetchNoteById } from '@/lib/api/api'
+import { fetchNoteById } from '@/lib/api/ClientApi'
 import type { Note } from '@/types/note'
 import Modal from "@/components/Modal/Modal"
 import { useRouter } from 'next/navigation';
